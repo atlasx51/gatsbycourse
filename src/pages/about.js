1e31d125b333a19878e96ebf53f199def1d505f1
atlasx51 <ericvarg84@gmail.com>
2020-05-29 18:51:20 -0700

    first time

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = ()=>{
+    return(
+        <div>
+            <p>Putting my thoughts and experiences out while learning a new craft.</p>
+        </div>
+    )
+}
