145c8d646d76e176fd7fbdbaa4ad4448e9375add
atlasx51 <ericvarg84@gmail.com>
2020-06-07 10:03:08 -0700

    stylin

--- src/component/header.js
+++ src/component/header.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import {Link} from 'gatsby'
+// import './header.module.scss'
+import headerStyles from './header.module.scss'
 
 const Header = () => {
   return (
     <div>
-      <header>
-         <h1>Test Blog</h1>
+      <header className={headerStyles.header}>
+         <h1>
+           <Link className={headerStyles.title} to='/'>
+           Test Blog 
+           </Link>
+         </h1>
          <nav>
            <ul>
              <li>
-             <Link to='/'>Home</Link>  
+             <Link className={headerStyles.link} to='/'>Home</Link>  
              </li>
              <li>
-             <Link to='/about'>About</Link>  
+             <Link className={headerStyles.link} to='/about'>About</Link>  
              </li>
              <li>
-             <Link to='/blog'>Blog</Link>  
+             <Link className={headerStyles.link} to='/blog'>Blog</Link>  
              </li>
              <li>
-             <Link to='/contact'>Contact</Link>  
+             <Link className={headerStyles.link} to='/contact'>Contact</Link>  
              </li>
            </ul>
          </nav>
