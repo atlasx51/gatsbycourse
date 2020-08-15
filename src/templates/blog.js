60989d1e352b91bc562e00aee2adea6cc20fc324
atlasx51 <ericvarg84@gmail.com>
2020-08-14 23:16:23 -0700

    rendered new blog page

---
+++ src/templates/blog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../component/layout'
+
+const Blog=()=>{
+    return(
+        <Layout>
+            This is the blog template
+        </Layout>
+
+    )
+}
+
+export default Blog
